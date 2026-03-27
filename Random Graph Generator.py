a2a87c5ce0a8ca46ba5a25c7371c19ba91234f60
lara <210695423+Lara-256@users.noreply.github.com>
2026-03-27 16:58:11 +0100

    Adding comments

--- Random Graph Generator.py
+++ Random Graph Generator.py
@@ -1,15 +1,19 @@
 import random
 from random import randrange
 
+#The following code shows the creation of a pseudo-random graph.
+
 def randomgraph(n,p):
-    graph = [[0]*n for i in range(n)]  # 1-based indexing
+    graph = [[0]*n for i in range(n)]  # create a n x n Matrix containing only 0's
 
     for i in range(n):
         for j in range(n):
-            if i<j and random.random() < p:
-                graph[i][j]=randrange(1,10)
+            if i<j and random.random() < p: #For each pair of nodes (i,j), an edge is created with a probability p
+                graph[i][j]=randrange(1,10) #The edges have a capacity randomly assigned between 1 and 10
 
     return graph
 
-rg=randomgraph(600,0.1)
-print(rg)
+
+#An example of a graph generation with n=600 and an edge probability of 10%. Delete the '#' to run it
+#rg=randomgraph(600,0.1)
+#print(rg)
